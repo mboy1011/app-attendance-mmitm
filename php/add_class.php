872475e22c6d666015bae6c0bcb2994a32205548
leabacus <bleajean@gmail.com>
2021-04-19 20:53:00 +0800

    added

--- php/add_class.php
+++ php/add_class.php
@@ -71,7 +71,15 @@ require('session.php');
               <div class="divider"></div>
             </div>
         </li>
-        <li><a href="#" class="white-text">Class <i class="small material-icons left white-text">school</i></a></li>
+        <li class="active">
+            <div class="collapsible-header white-text"><i class="material-icons right white-text">people_alt</i>Class</div>
+            <div class="collapsible-body white darken-5">
+              <a href="add_class.php" class="green-text"><i class="material-icons left green-text" style="font-size:25px; padding-left: 50px;">group_add</i> Add Class</a>
+              <div class="divider"></div>
+              <div><a href="view_class.php" class="green-text"><i class="material-icons left green-text" style="font-size:25px; padding-left: 50px;">list</i> View Classes</a></div>
+              <div class="divider"></div>
+            </div>
+        </li>
         <li>
             <div class="collapsible-header white-text"><i class="material-icons right white-text">assignment_ind</i>Faculty</div>
             <div class="collapsible-body white darken-5">
@@ -81,7 +89,7 @@ require('session.php');
               <div class="divider"></div>
             </div>
         </li>
-        <li class="active">
+        <li>
             <div class="collapsible-header white-text"><i class="material-icons right white-text">badge</i>Users</div>
             <div class="collapsible-body white darken-5">
               <a href="add_user.php" class="green-text"><i class="material-icons left green-text" style="font-size:25px; padding-left: 50px;">group_add</i> Add Users</a>
